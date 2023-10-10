427aa1323a1cccce1065c608ce6e012e45ecc465
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-10-10 22:10:31 +0300

    fix

--- src/components/LastArticle.vue
+++ src/components/LastArticle.vue
@@ -1,6 +1,6 @@
 <template>
-
-      <div @click="router.push( `/article/${ item.id }/${ item.name.split(' ').join('-') }` ).then(reload )"
+   <RouterLink :to=" { name: 'article', params: { name: ( item.name ).split( ' ' ).join( '-' ), id: item.id } }">
+      <div
       class="cursor-pointer flex rounded-[15px] overflow-hidden border border-[#BFBFBF] gap-2 md:min-h-[125px] lg:min-h-[150px]   xl:min-h-[175px] max-h-[175px] aspect-[2/.8] my-2 ">
       <div class=" aspect-square object-cover">
          <img class="w-full aspect-square " :src="item.url" alt=" article photo">
@@ -11,18 +11,13 @@
          <p  class=" line-clamp-3 leading-7 text-sm">{{ item.content }}</p>
       </div>
    </div>
-   
+   </RouterLink>
 
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
 
-const router = useRouter()
-
-const reload = () => {
-   window.location.reload()
-}
 
 defineProps( {
    item: {
